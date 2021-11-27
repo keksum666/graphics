8e2b90b8b15f264c65383093bf5cc73603dddfb0
keksum666 <keksum666@yandex.ru>
2021-11-27 22:43:37 +0400

    Fix

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <VueForceGraphVR :graphData="graphData" backgroundColor="#090723" style="height: 800px"></VueForceGraphVR>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import { VueForceGraphVR } from "vue-force-graph/es/components/vue-force-graph-vr";
+
+export default {
+  name: "TestComponent",
+  components: {
+    VueForceGraphVR,
+  },
+  data: () => ({
+    graphData: {
+      "nodes": [
+      ],
+      "links": [
+      ]
+    }
+  }),
+  async created(){
+    await axios.get('http://localhost:8000/api/module/articles/graph-data').then((response) => {
+      this.graphData = response.data;
+    })
+  }
+}
+</script>
+
+<style scoped>
+.graph-nav-info {
+  height: 800px !important;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "vue-force-graph/index.css"
+import LoadScript from 'vue-plugin-load-script'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+app.mount('#app')
